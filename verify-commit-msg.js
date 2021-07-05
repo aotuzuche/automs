5a7f5b9f35d62b6882f42ba3403b5aa29cf3c05a
Eason chiu <76670758@qq.com>
2021-07-06 01:13:45 +0800

    test

--- verify-commit-msg.js
+++ verify-commit-msg.js
@@ -1,7 +1,11 @@
 const chalk = require('chalk')
+const fs = require('fs')
 
-const msgPath = process.env.HUSKY_GIT_PARAMS
-const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
+if (!process.argv[2] || !fs.existsSync(process.argv[2])) {
+  return
+}
+
+const msg = fs.readFileSync(process.argv[2], 'utf-8').trim()
 
 const commitRE =
   /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build|v)(\(.+\))?: .{1,50}/
@@ -9,5 +13,6 @@ const commitRE =
 if (!/^v\d+\.\d+\.\d+/.test(msg) && !commitRE.test(msg)) {
   console.log()
   console.error(`  ${chalk.bgRed.white(' ERROR ')} 无效的提交信息`)
+  console.log()
   process.exit(1)
 }
