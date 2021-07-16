af4938e718c769f622d194ed2b681d34da14c12b
Eason chiu <76670758@qq.com>
2021-07-16 14:48:42 +0800

    fix: fix sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'publish with proxy:'
-git config --global https.proxy socks5://127.0.0.1:10081
-git config --global https.proxy
+git config --global https.https://github.com.proxy socks5://127.0.0.1:10081
+git config --global https.https://github.com.proxy
 ./node_modules/lerna/cli.js publish
-git config --global --unset https.proxy
+git config --global --unset https.https://github.com.proxy
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'push with proxy:'
-git config --global https.proxy socks5://127.0.0.1:10081
-git config --global https.proxy
+git config --global https.https://github.com.proxy socks5://127.0.0.1:10081
+git config --global https.https://github.com.proxy
 git push
-git config --global --unset https.proxy
+git config --global --unset https.https://github.com.proxy
