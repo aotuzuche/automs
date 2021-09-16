a5306aeac9929b163a0cf262c494bce2b9c48fc3
Eason chiu <76670758@qq.com>
2021-09-16 14:50:54 +0800

    fix: fix bug

--- packages/tools/scripts/generateProject.js
+++ packages/tools/scripts/generateProject.js
@@ -1,6 +1,8 @@
-const fs = require('fs-extra')
 const path = require('path')
+const fs = require('fs-extra')
 const os = require('os')
+const paths = require('../libs/paths')
+const logger = require('../libs/logger')
 
 const generateProject = projectPath => {
   const pkg = {
@@ -13,21 +15,26 @@ const generateProject = projectPath => {
   const packageJson = path.resolve(projectPath, 'package.json')
   fs.writeFileSync(packageJson, JSON.stringify(pkg, null, 2) + os.EOL)
 
+  console.log('')
+  createFile('_dot_babelrc', '.babelrc')
+  createFile('_dot_editorconfig', '.editorconfig')
+  createFile('_dot_env', '.env')
+  createFile('_dot_eslintrc_dot_js', '.eslintrc.js')
+  createFile('_dot_gitignore', '.gitignore')
+  createFile('_dot_lintstagedrc', '.lintstagedrc')
+  createFile('_dot_prettierrc', '.prettierrc')
+  createFile('_dot_stylelintrc_dot_json', '.stylelintrc.json')
+}
+
+const createFile = (source, target) => {
   const template = path.resolve(require.resolve('@automs/template'), '..')
-  const initTemplate = path.resolve(template, 'init')
+  const f = path.resolve(template, 'init', source)
 
-  console.log(process.cwd())
-  console.log(__dirname)
-  console.log(initTemplate)
-
-  // const res = spawn.sync('yarn', ['add', '@automs/template', '-D'], {
-  //   stdio: 'inherit',
-  // })
-
-  // if (res.status !== 0) {
-  //   console.log(res)
-  //   throw res
-  // }
+  if (fs.existsSync(f)) {
+    const str = String(fs.readFileSync(f))
+    fs.writeFileSync(path.resolve(paths.appPath, target), str)
+    logger.succeed('创建' + target)
+  }
 }
 
 module.exports = generateProject
